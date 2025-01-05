4c4504fa7471a8c7d1fb905027100314d0548b31
zhanggang <95312@163.com>
2025-01-05 20:29:00 +0800

    字符串切片

--- src/main.rs
+++ src/main.rs
@@ -7,12 +7,14 @@ fn main() {
     // 默认不可变：在 Rust 中，变量默认是不可变的。这意味着一旦变量被赋值后，就不能再改变它的值，除非显式声明为可变的。
     // 可变性：使用 mut 关键字可以声明一个可变变量。
     let s1 = 100;
-    let mut s2 ="hello world";
+    let s2 ="hello world";
+    println!("s1 = {}, s2 = {}", s1, s2);
 
     // 字符串
     let s3 = String::from("hello world");
     // 字符串切片
-    let mut s2 ="hello world";
+    let s4 ="hello";
+    println!("s3 = {}, s4 = {}", s3, s4);
 
 
 
